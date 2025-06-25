f7103f078762620313f0ec4ae8776d886aeccba1
LautaroMalleret <lautaromalleret@gmail.com>
2025-06-24 21:20:15 -0300

    cambio de numero

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
 //definimos las constantes de la aplicacion
 // Estas constantes se utilizan en diferentes partes de la aplicación, como el footer y las redes sociales
-export const WHATSAPP_NUM = "1132737716";
+export const WHATSAPP_NUM = "541132737716";
 export const INSTAGRAM_URL = "https://www.instagram.com/ropa_hombre_mujer_chicosychica/"
